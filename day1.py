b38752a76fd723be7ac3ced4f46adf2bf552120d
Lavanya411 <60595160+Lavanya411@users.noreply.github.com>
2021-04-13 17:01:30 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("lavanya")
+
+
+# In[4]:
+
+
+print("title.lavanya")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
